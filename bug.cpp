b5e18722efcef0d1ca8aa5c12e6ca935813a0c03
BugHunter-X <askzenai@gmail.com>
2025-01-20 04:26:05 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,9 @@
+std::vector<int> vec; 
+for (int i = 0; i < 10; ++i) {
+  vec.push_back(i); 
+}
+
+for (int i = 0; i < vec.size(); ++i) {
+  vec.erase(vec.begin() + i);
+}
+//This code intends to clear the vector but has a bug. 
